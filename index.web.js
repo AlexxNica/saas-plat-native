184350b7e659e0a97228a60c8033ee87a688d155
saas_plat@163.com <saas_plat@163.com>
2017-04-01 11:13:30 +0800

    web移除，独立项目，和native方式不同

--- index.web.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
-import './spdefine';
-import './bundles';
-
-ReactDOM.render(App, document.getElementById('saas-plat'));
